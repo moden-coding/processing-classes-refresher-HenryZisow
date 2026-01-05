7fc6fb4dac83d79006aa895be300572e7dfe81b8
HenryZisow <28hzisow@gannacademy.org>
2026-01-05 11:39:22 -0500

    Done

--- src/App.java
+++ src/App.java
@@ -1,29 +1,37 @@
 import processing.core.*;
 import java.util.*;
 
-public class App extends PApplet{
+public class App extends PApplet {
     ArrayList<Enemy> enemies = new ArrayList<>();
+    private float r;
+    private float g;
+    private float b;
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         PApplet.main("App");
     }
 
-    public void settings(){
-        size(800,600);
+    public void settings() {
+        size(800, 600);
     }
 
-    public void setup(){
-
+    public void setup() {
+        r = random(0, 255);
+        g = random(0, 255);
+        b = random(0, 255);
     }
 
-    public void draw(){
-
-
+    public void draw() {
     }
 
-    public void mousePressed(){
-        
+    public void mousePressed() {
+        fill(r,g,b);
+        float x = mouseX;
+        float y = mouseY;
+        Enemy enemy = new Enemy(this, x, y);
+        enemies.add(enemy);
+        for (Enemy i : enemies) {
+            rect(x, y, 50, 50);
+        }
     }
-
-
 }
--- src/Enemy.java
+++ src/Enemy.java
@@ -1,18 +1,16 @@
 
 import processing.core.*;
+
 public class Enemy {
     private PApplet screen;
     private float x, y, width, height;
 
-    public Enemy(PApplet s, float xPos, float yPos){
+    public Enemy(PApplet s, float xPos, float yPos) {
         screen = s;
         x = xPos;
         y = yPos;
-        width = 150;
-        height = 75;
+        width = 15;
+        height = 10;
 
     }
-
-    
-    
 }
